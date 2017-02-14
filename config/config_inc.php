8efe334c775bcff15b1f67a18f2236c21c35b325
Nathalie <nathalie@hollandshielding.com>
2017-02-14 13:31:33 +0100

    Initial commit

---
+++ config/config_inc.php
@@ -0,0 +1,10 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = 'Eequi4';
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = 'v1h+xirY5Si1/iG5IoQJh2wFcjaBeqnNYjWta/eysh8=';
